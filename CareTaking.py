1b4ca4ab0eae75ca3cb7638b739e5beee221b11b
SrinidDev <45865576+SrinidDev@users.noreply.github.com>
2020-01-21 19:51:04 +0530

    Update CareTaking.py

--- CareTaking.py
+++ CareTaking.py
@@ -1,6 +1,6 @@
 import mysql.connector
 
-mydb = mysql.connector.connect(host="localhost", user="root", passwd="root", database="Careall")
+mydb = mysql.connector.connect(host="localhost", user="root", passwd="root", database="EdYoda")
 
 mycursor = mydb.cursor()
 mycursor.execute("SELECT Yname,Ename FROM Youth,Elder,YCaresE WHERE Youth.Yid=YCaresE.Yid AND Elder.Eid=YCaresE.Eid;")
